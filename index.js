69dd8ac6389dd459ca606b22de208c793188c12b
Aviv Hadar <Avivhdr@gmail.com>
2018-08-15 21:43:34 +0300

    Add validation to onRemove
    this addition solves a bug caused when using markerWithLaber together with clustering in certain conditions.

--- index.js
+++ index.js
@@ -322,12 +322,16 @@ module.exports = function(gMapsApi) {
    */
   MarkerLabel_.prototype.onRemove = function () {
     var i;
-    this.labelDiv_.parentNode.removeChild(this.labelDiv_);
-    this.eventDiv_.parentNode.removeChild(this.eventDiv_);
-
+    if (this.labelDiv_.parentNode) {
+      this.labelDiv_.parentNode.removeChild(this.labelDiv_);
+      this.eventDiv_.parentNode.removeChild(this.eventDiv_);
+    }
+    
     // Remove event listeners:
-    for (i = 0; i < this.listeners_.length; i++) {
-      gMapsApi.event.removeListener(this.listeners_[i]);
+    if (this.listeners_) {
+      for (i = 0; i < this.listeners_.length; i++) {
+        gMapsApi.event.removeListener(this.listeners_[i]);
+      }
     }
   };
 
